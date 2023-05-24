440e22d35e83a243cd77f40c0d4c1e64747523ce
Cristian Quito <cristianandresquito@gmail.com>
2023-05-24 07:35:24 -0500

    feat: user domain rules added

---
+++ LearningCenter.Domain/IUserDomain.cs
@@ -0,0 +1,10 @@
+﻿using LearningCenter.Infrastructure.Models;
+
+namespace LearningCenter.Domain;
+
+public interface IUserDomain
+{
+    public bool Save(User user);
+    public bool Update(int id, User user);
+    public bool Delete(int id);
+}
