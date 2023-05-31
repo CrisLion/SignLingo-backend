c2c08d9b78b97f29b8e160e81f90a112182ee380
William Riega <williammar954@hotmail.com>
2023-05-31 11:34:06 -0500

    feat: Tables Answer, Exercise and Module created

--- LearningCenter.Infrastructure/Models/Exercise.cs
+++ LearningCenter.Infrastructure/Models/Exercise.cs
@@ -4,6 +4,6 @@ public class Exercise
 {
     public int Id { get; set; }
     public string Question { get; set; }
-    public List<string> Answer { get; set; }
+    public List<Answer> Answers { get; set; }
     public int ModuleId { get; set; }
 }
--- LearningCenter.Infrastructure/Models/Module.cs
+++ LearningCenter.Infrastructure/Models/Module.cs
@@ -3,6 +3,6 @@
 public class Module
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Module_Name { get; set; }
     public List<Exercise> Exercises { get; set; }
 }
